33dd51ba4cbd6c99cb9450f3f8fbd73835d9ad3d
romanaverkin <romanaverkin@gmail.com>
2020-07-14 21:14:16 +0300

    Making things look better

--- src/main.py
+++ src/main.py
@@ -1,2 +1,25 @@
+import numpy as np
+from matplotlib import pyplot as plt
 
+from src.robustness import feature_
+
+# x = np.arange(0, 4 * np.pi, 0.1)  # start,stop,step
+# signal = np.sin(x)
+#
+# plt.plot(range(122),  list(feature_(signal, (1, 5))), signal)
+# plt.show()
+
+from scipy import signal
+import matplotlib.pyplot as plt
+t = np.linspace(0, 1, 500, endpoint=False)
+sig = signal.square(2 * np.pi * 5 * t)
+exp = list(feature_(sig, (10, 20)))
+print(sig)
+print(exp)
+
+
+plt.plot(range(472), exp,  sig)
+# plt.ylim(-2, 2)
+plt.show()
+#
 
--- test/test_robustness.py
+++ test/test_robustness.py
@@ -19,16 +19,3 @@ def test_or():
 def test_feature():
     assert list(rb.feature_(range(0, 20), (1, 2))) == \
            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-
-# x = np.arange(0, 4 * np.pi, 0.1)  # start,stop,step
-# signal = np.sin(x) + np.cos(5*x)
-#
-# print(len(signal))
-# print(len(list(feature_(signal, (10, 20)))))
-# plt.plot(range(98),  list(feature_(signal, (10, 20))), signal)
-# x = np.arange(0, 4 * np.pi, 0.1)  # start,stop,step
-# sig = np.cos(x * 5) + np.sin(x)
-
-
-# plt.plot(x, list(not_(sin_y)))
-# plt.show()

77334cd792052b92ff8ad41073b2d23f3f39fef9
romanaverkin <romanaverkin@gmail.com>
2020-07-25 19:15:05 +0300

    working on

--- src/main2.py
+++ src/main2.py
@@ -1,8 +1,17 @@
-
-
 import pandas as pd
 import src.robustness as rb
+import matplotlib.pyplot as plt
 
-frame = pd.read_csv('Time_Series_Metrics.csv')
+frame = pd.read_csv('Time_Series_Metrics2.csv')
 
-print(list(rb.until( frame['Host 1 Distance traveled until current frame [m]'], (0,100), frame['Host 1 Distance traveled until current frame [m]'])))
+ego_speed = frame['Host 1 Ego Proj x Speed [m/s]']
+actor_speed = frame['Player 2 Self Proj x Speed [m/s]']
+distance_to_actor = frame['Host 1 X coordinate distance to closest Player in front bbox [m]']
+speed_limit = [10 for i in ego_speed]
+
+distance_limit = distance_to_actor -.1
+
+
+until = list(rb.until(ego_speed, [0, 10], distance_limit))
+plt.plot(range(len(until)), until, 0)
+plt.show()
